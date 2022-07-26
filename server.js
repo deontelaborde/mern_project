567833272e3cb613866c1382c65dec8e5d0b19d9
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-26 12:20:44 -0400

    created server route to songs database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001
 const db = require('./db')
 
 const app = express()
+const { Song } = require('./models')
 
 app.use(cors())
 app.use(express.json())
@@ -12,6 +13,11 @@ app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.send('This is root!')
+})
+// server.js
+app.get('/songs', async (req, res) => {
+  const songs = await Song.find({})
+  res.json(songs)
 })
 
 app.listen(PORT, () => {
