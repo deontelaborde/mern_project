f6b272a20e152c17c1deaa33e28d897a5b2c370d
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-26 12:32:21 -0400

    created server route to each song's id

--- server.js
+++ server.js
@@ -14,10 +14,22 @@ app.use(logger('dev'))
 app.get('/', (req, res) => {
   res.send('This is root!')
 })
-// server.js
+
 app.get('/songs', async (req, res) => {
   const songs = await Song.find({})
   res.json(songs)
+})
+
+app.get('/songs/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const song = await Song.findById(id)
+    if (!song) throw Error('Song not found')
+    res.json(song)
+  } catch (e) {
+    console.log(e)
+    res.send('Song not found!')
+  }
 })
 
 app.listen(PORT, () => {
