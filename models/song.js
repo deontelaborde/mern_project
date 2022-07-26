63584be7f82c59d1ab14dd717fe1cb7cedcc5d8f
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-26 09:37:09 -0400

    created song schema

---
+++ models/song.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose')
+
+const Song = new Schema(
+  {
+    title: { type: String, required: true },
+    producer: { type: String, required: true },
+    produced_date: { type: String, required: true },
+    producer_id: { type: Schema.Types.ObjectId, ref: 'producer_id' }
+  },
+  { timestamps: true }
+)
+
+module.exports = Song
