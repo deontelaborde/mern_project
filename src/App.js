d8b221b680906f32630f4dcd0e8e9b400ff323d1
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-28 14:16:14 -0400

    created App.js outline

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { userState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+return (
+  <div className="App">
+    <header>{/* Import Nav here */}</header>
+    <main>
+      <Routes></Routes>
+    </main>
+  </div>
+)
