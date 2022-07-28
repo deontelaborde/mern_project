557e837bfd93479314318ffa1a6c1bd518477676
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-28 16:47:34 -0400

    added useEffect to access db in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import './App.css'
-import React from 'react'
-// import { Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Nav from './components/Nav'
+// import Library from './components/Library'
+import axios from 'axios'
 
 function App() {
+  useEffect(() => {
+    async function getSonglist() {
+      const res = await axios.get(`http://localhost:3001/api/songs`)
+      console.log(res)
+    }
+    getSonglist()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">{<Nav />}</header>
       <main>
-        <h1>Hi</h1>
-        {<Home />}
-
-        {/* <Routes>
-        
-        </Routes> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/Library" element={<Library />} /> */}
+        </Routes>
       </main>
     </div>
   )
