f7f0bd8f23cf02c171ba41fa5ef6a5f3c988807b
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-28 10:50:58 -0400

    created getSongById controller

--- controllers/index.js
+++ controllers/index.js
@@ -11,7 +11,8 @@ const createSong = async (req, res) => {
     return res.status(500).json({ error: error.message })
   }
 }
-const getAllSong = async (req, res) => {
+
+const getAllSongs = async (req, res) => {
   try {
     const songs = await Song.find({})
     return res.status(200).json({ songs })
@@ -19,5 +20,17 @@ const getAllSong = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
+const getSongById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const song = await Song.findById(id)
+    if (song) {
+      return res.status(200).json({ song })
+    }
+    return res.status(404).send('Song with stated ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
-module.exports = { createSong, getAllSongs }
+module.exports = { createSong, getAllSongs, getSongById }
