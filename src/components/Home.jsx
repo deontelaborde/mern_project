61f250a7058543d72932f4f2a5b2787d1d46e74d
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-28 14:22:00 -0400

    created Home.jsx component

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+
+  return (
+    <div className="home">
+      <div>
+        <img
+          style={{ display: 'block', maxWidth: '100%' }}
+        />
+      </div>
+      <div>
+        <h1>MWDBeats</h1>
+        <h2>
+          You Know the Vibes!!
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
