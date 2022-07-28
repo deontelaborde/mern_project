768a8a4f655efd81c69f6c4e34b90b90cddac8e7
Deonte LaBorde <deonte.laborde@gmail.com>
2022-07-28 16:22:30 -0400

    linked react-router-dom to react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import './App.css'
 import React from 'react'
 // import { Routes, Route } from 'react-router-dom'
-// import Home from './components/Home'
-// import Nav from './components/Nav'
+import Home from './components/Home'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">{/* <Nav /> */}</header>
+      <header className="App-header">{<Nav />}</header>
       <main>
         <h1>Hi</h1>
+        {<Home />}
+
         {/* <Routes>
-          <Route path="/" element={<Home />} />
+        
         </Routes> */}
       </main>
     </div>
